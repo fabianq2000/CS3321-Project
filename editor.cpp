95be18845b98066309ee3f13abac475973f85c07
wardaqadeer-30 <88466200+wardaqadeer-30@users.noreply.github.com>
2021-08-12 20:35:30 -0500

    Update editor.cpp

--- editor.cpp
+++ editor.cpp
@@ -1,1 +1,48 @@
+# include <iostream>
+# include <string>
+# include <fstream>
+# include "editor.h"
 
+using namespace std;
+
+void testOutput(std::string output)
+{
+	cout << "test output" << endl;
+	cin >> output;
+}
+
+string createFiles(string Files)
+{
+	string input_converter;
+	Files = CreateFiles.getinput_converter(files);
+	return files;
+}
+
+string createTextFiles(string text)  //The initial text files after conversion return here 
+{
+	return text;
+}
+
+string inputSampleText(string sample text)  //This will prompt inputting creative texts for pdf documents
+{
+	return sample text;
+}
+
+
+string options = "Scroll up";
+string options = "Scroll down";
+string options = "Edit line";
+string options = "Delete line";
+string options = "Append line";
+string options = "Format line";
+string options = "Save and quit";
+string fullName = firstName + lastName;
+cout << getOptions;
+
+
+
+
+int main()
+{
+	editor;
+}
